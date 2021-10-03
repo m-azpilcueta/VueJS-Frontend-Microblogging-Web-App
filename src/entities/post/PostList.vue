5cb5b9c3d493422403037c9058f6997effd1c045
martinazpilcueta <m.azpilcueta@udc.es>
2021-10-03 15:54:03 +0200

    Boton oculto para usuarios no autenticados

--- src/entities/post/PostList.vue
+++ src/entities/post/PostList.vue
@@ -6,7 +6,7 @@
           <v-col cols="3"></v-col>
           <v-col cols="4"><h1>Blog Posts</h1></v-col>
           <v-col cols="3">
-            <v-btn v-if="!isAdmin" :to="{ name: 'PostCreate' }" color="primary">
+            <v-btn v-if="!isAdmin && isLogged" :to="{ name: 'PostCreate' }" color="primary">
               <v-icon>mdi-plus</v-icon>
               <span class="d-none d-md-flex">Create post</span>
             </v-btn>
@@ -33,6 +33,9 @@ export default {
   computed: {
     isAdmin() {
       return store.state.user.authority == "ADMIN";
+    },
+    isLogged() {
+      return store.state.user.logged;
     },
   },
   async mounted() {
